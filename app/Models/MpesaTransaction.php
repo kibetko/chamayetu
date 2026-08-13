214cb1242de45d8f1e5e8e51c6ec29b296bbc048
kibeert <testdevvic20@gmail.com>
2026-08-13 19:23:58 +0800

    Update Mpesa Transaction php

--- app/Models/MpesaTransaction.php
+++ app/Models/MpesaTransaction.php
@@ -10,9 +10,7 @@ class MpesaTransaction extends Model
 
     public function user()
     {
-        return $this->belongsTo(
-            User::class
-        );
+        return $this->belongsTo(User::class);
     }
 
     public function contribution()
